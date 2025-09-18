fa05dafc039a93bbec3302357db5596a88e5de0a
David Medina <david@davidmedina.es>
2025-09-18 12:48:33 +0200

    Fix UploadHandlersProvider import error in admin layout

--- src/app/(payload)/admin/[[...segments]]/layout.tsx
+++ src/app/(payload)/admin/[[...segments]]/layout.tsx
@@ -1,8 +1,6 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import type { Metadata } from 'next'
-import { UploadHandlersProvider } from '@payloadcms/next/layouts'
-import { VercelBlobClientUploadHandler } from '@payloadcms/storage-vercel-blob/client'
 
 import config from '@payload-config'
 import { generatePageMetadata } from '@payloadcms/next/views'
@@ -20,16 +18,6 @@ type Args = {
 export const generateMetadata = ({ params, searchParams }: Args): Promise<Metadata> =>
   generatePageMetadata({ config, params, searchParams })
 
-const Layout = ({ children, params, searchParams }: Args) => (
-  <UploadHandlersProvider
-    handlers={[
-      {
-        handler: VercelBlobClientUploadHandler,
-      },
-    ]}
-  >
-    {children}
-  </UploadHandlersProvider>
-)
+const Layout = ({ children }: Args) => children
 
 export default Layout
